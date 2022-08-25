674af2d6b2140f7156536d6ba263b8c2acc824d8
adityackr <aditya09me@gmail.com>
2022-08-25 14:27:41 +0600

    Add: property descriptor

--- 02-prototype/app.js
+++ 02-prototype/app.js
@@ -1,6 +1,32 @@
-function Person(name) {
-	this.name = name;
-}
+// function Person(name) {
+// 	this.name = name;
+// }
 
-const person = new Person('Aditya');
-console.log(person);
+// const person = new Person('Aditya');
+// console.log(person);
+
+const person = {
+	name: 'Aditya',
+};
+
+// console.log(person);
+
+// for (const i in person) {
+// 	console.log(i);
+// }
+
+// console.log(Object.keys(person));
+
+// const descriptor = Object.getOwnPropertyDescriptor(person, 'name');
+// console.log(descriptor);
+
+// const baseObj = Object.getPrototypeOf(person);
+// const descriptor = Object.getOwnPropertyDescriptor(baseObj, 'toString');
+// console.log(descriptor);
+
+Object.defineProperty(person, 'name', {
+	writable: false,
+	enumerable: false,
+	configurable: false,
+	value: 'Aditya Chakraborty',
+});
