9b8241c43d4c9605d92336ef9bc38cd96e971d0c
adityackr <aditya09me@gmail.com>
2022-08-25 16:12:12 +0600

    Add: Instance vs Prototype members

--- 02-prototype/app.js
+++ 02-prototype/app.js
@@ -5,9 +5,9 @@
 // const person = new Person('Aditya');
 // console.log(person);
 
-const person = {
-	name: 'Aditya',
-};
+// const person = {
+// 	name: 'Aditya',
+// };
 
 // console.log(person);
 
@@ -24,9 +24,37 @@ const person = {
 // const descriptor = Object.getOwnPropertyDescriptor(baseObj, 'toString');
 // console.log(descriptor);
 
-Object.defineProperty(person, 'name', {
-	writable: false,
-	enumerable: false,
-	configurable: false,
-	value: 'Aditya Chakraborty',
-});
+// Object.defineProperty(person, 'name', {
+// 	writable: false,
+// 	enumerable: false,
+// 	configurable: false,
+// 	value: 'Aditya Chakraborty',
+// });
+
+// function Person(name) {
+// 	this.name = name;
+// }
+
+// Person.prototype.PI = 3.1416;
+
+// const p1 = new Person('Aditya');
+// const p2 = new Person('Aditya Chakraborty');
+
+// console.log(Object.getPrototypeOf(p1));
+// console.log(Person.prototype);
+
+function Square(width) {
+	this.width = width;
+}
+
+Square.prototype = {
+	draw: function () {
+		console.log('Draw');
+	},
+	toString: function () {
+		return 'My width is ' + this.width;
+	},
+};
+
+const sqr1 = new Square(10);
+const sqr2 = new Square(5);
